3238ab2630ba62fc1fa27179eee9de15663066b5
dl <dl@AKSHAYs-MacBook-Air.local>
2019-08-11 22:38:35 +0530

    Updates

---
+++ precache-manifest.9bf20205e5026d44bbbe85e974719679.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb689b6bfaa221bc8a7d12da606e3a0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c980682552b3fb3d5087",
+    "url": "/monsters-rolodex/static/css/main.eb338f4b.chunk.css"
+  },
+  {
+    "revision": "c23405c3eed018c19ba6",
+    "url": "/monsters-rolodex/static/js/2.31bd22d1.chunk.js"
+  },
+  {
+    "revision": "c980682552b3fb3d5087",
+    "url": "/monsters-rolodex/static/js/main.56126937.chunk.js"
+  },
+  {
+    "revision": "ed45351d6e7cff73c95b",
+    "url": "/monsters-rolodex/static/js/runtime~main.aece0cbc.js"
+  }
+]);
